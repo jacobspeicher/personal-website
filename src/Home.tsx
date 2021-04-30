677478b3b441e7b71a2799459c4e4fd16a33235b
YOUR NAME <speicherjacob@gmail.com>
2021-04-30 15:58:47 -0500

    changed homepage

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <header className="homeHeader">Jacob Speicher</header>
+    </div>
+  );
+}
+
+export default Home;
