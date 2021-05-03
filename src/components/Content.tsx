b8863266bfe35ade656b0d167f9d3a40b1c2efb2
YOUR NAME <speicherjacob@gmail.com>
2021-05-03 13:26:20 -0500

    fixing stupid linter errors

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -13,8 +13,7 @@ function Content(props: { page: CurrentPage }) {
       <div className="content">
         {props.page === CurrentPage.Home && (
           <div className="intro">
-            Hey, I'm a software developer and game developer in
-            Madison, WI.
+            Hey, I'm a software developer and game developer in Madison, WI.
           </div>
         )}
         {props.page === CurrentPage.Games && (
